bd86062fc7d510e84a7ca6447a0859c5f66de976
Ryosuke Suzuki <ryosukeclarinet@gmail.com>
2017-07-05 23:42:03 +0900

    Update start_big_jupyter.sh

--- start_big_jupyter.sh
+++ start_big_jupyter.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8888:8888 --name jupyter -v ./:/home/jovyan/work jupyter/datascience-notebook start-notebook.sh --NotebookApp.token=''  -e GRANT_SUDO=yes
+docker run -d -p 8888:8888 --name big_jupyter -v ./:/home/jovyan/work jupyter/datascience-notebook start-notebook.sh --NotebookApp.token=''  -e GRANT_SUDO=yes
